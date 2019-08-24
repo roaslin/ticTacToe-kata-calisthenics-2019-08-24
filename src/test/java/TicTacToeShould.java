fc72bed86cfdeda79bbaa505dff023a472acdcf6
roaslin <roaslin@gmail.com>
2019-08-24 22:35:31 +0200

    refactor tictactoe

--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -1,6 +1,8 @@
+import org.junit.Before;
 import org.junit.Test;
 
 public class TicTacToeShould {
+    private TicTacToe ticTacToe;
 
 //    X always goes first.
 //    Players alternate placing X’s and O’s on the board.
@@ -8,21 +10,23 @@ public class TicTacToeShould {
 //    A player with three X’s or O’s in a row (horizontally, vertically, diagonally) wins.
 //    If all nine squares are filled and neither player achieves three in a row, the game is a draw.
 
+    @Before
+    public void setUp() throws Exception {
+        ticTacToe = new TicTacToe();
+    }
+
     @Test(expected = PlayerXPlacesFirst.class)
     public void not_allow_that_player_O_places_first() {
-        TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.place(Player.O);
     }
 
     @Test
     public void allow_player_X_place_first() {
-        TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.place(Player.X);
     }
 
     @Test
     public void alternate_players() {
-        TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.place(Player.X);
         ticTacToe.place(Player.O);
         ticTacToe.place(Player.X);
